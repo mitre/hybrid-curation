fdea88fec5a9cda0fd5535600b811a17927ab932
John Burger <john@mitre.org>
2017-04-24 10:39:35 -0400

    Major cleanup, including computing 'adjusted' work time based on overlapping durations from same Turker

--- src/unbundle-hits.py
+++ src/unbundle-hits.py
@@ -5,6 +5,10 @@ import types
 import csv
 import json
 import optparse
+import fileinput
+import collections
+import datetime
+import time
 
 """
 Essentially reverses the process of bundle-items.
@@ -29,36 +33,48 @@ def maybeOpen (file, mode="r", encoding="utf8"):
 
 ######################################################################
 
-class batchFileReader:
+# class batchFileReader:
 
-    def __init__ (self, file):
-        self.csvReader = csv.DictReader(maybeOpen(file, "r", None))
+#     def __init__ (self, file):
+#         self.csvReader = csv.DictReader(maybeOpen(file, "r", None))
 
-    wsRE = re.compile(r"\s") # re.U # NO!!!
+#     wsRE = re.compile(r"\s") # re.U # NO!!!
 
-    def __iter__ (self):
-        n = 0
-        for row in self.csvReader:
-            n += 1
-            for key, old in row.items():
-                if old:
-                    new = self.wsRE.sub(" ", row[key])
-                    if new is not old:
-                        row[key] = new      
-            yield row
-        # print >>sys.stderr, self, self.csvReader.fieldnames
-        print >>sys.stderr, "%s: %d" % (self, n)
+#     def __iter__ (self):
+#         n = 0
+#         for row in self.csvReader:
+#             n += 1
+#             for key, old in row.items():
+#                 if old:
+#                     new = self.wsRE.sub(" ", row[key])
+#                     if new is not old:
+#                         row[key] = new      
+#             yield row
+#         # print >>sys.stderr, self, self.csvReader.fieldnames
+#         print >>sys.stderr, "%s: %d" % (self, n)
 
-class hitUnbundler:
+wsRE = re.compile(r"\s") # re.U # NO!!!
+def readBatchFile (input):
+    n = 0
+    for n, row in enumerate(csv.DictReader(input), 1):
+        for key, old in row.items():
+            if old:
+                new = wsRE.sub(" ", row[key])
+                if new is not old:
+                    row[key] = new      
+        yield row
+    # print >>sys.stderr, self, self.csvReader.fieldnames
+    print >>sys.stderr, "%d batch items read" % n
 
-    def __init__ (self, source, burstplain=False, addSequenceID=False):
-        self.source = source
-        self.addSequenceID = addSequenceID
-        self.splitKeyRE = re.compile(burstplain and "^(.*[^0-9])([0-9]+)$" or "^(.+)_([0-9]+)$",
-                                     re.U | re.I)
+# class hitUnbundler:
 
-    def burst (self, bundle):
-        splitKeyRE = self.splitKeyRE
+#     def __init__ (self, source, burstplain=False, addSequenceID=False):
+#         self.source = source
+#         self.addSequenceID = addSequenceID
+#         self.splitKeyRE = re.compile(burstplain and "^(.*[^0-9])([0-9]+)$" or "^(.+)_([0-9]+)$",
+#                                      re.U | re.I)
+
+def burstBundle (bundle, splitKeyRE):
         burst = {}      # Maps sequence number to attributes with that numeric suffix
         shared = {}     # Attributes without a numeric suffix
         for key in bundle:
@@ -75,16 +91,18 @@ class hitUnbundler:
                 shared[key] = bundle[key]
         return burst, shared
 
-    def __iter__ (self):
+def unbundleHITs (source, burstplain=False, addSequenceID=False):
+        splitKeyRE = re.compile(burstplain and "^(.*[^0-9])([0-9]+)$" or "^(.+)_([0-9]+)$",
+                                     re.U | re.I)
         nIn = nOut = 0
 #       indexCount = {}
-        for bundle in self.source:
+        for bundle in source:
             nIn += 1
 #           tempIndex = {}
 #           for index in tempIndex:
 #               indexCount[index] = indexCount.get(index, 0) + 1
-            burst, shared = self.burst(bundle)
-            if self.addSequenceID:
+            burst, shared = burstBundle(bundle, splitKeyRE)
+            if addSequenceID:
                 for index, subBundle in burst.iteritems():
                     subBundle["sequenceID"] = index
             for item in burst.values() or [{}]:
@@ -96,9 +114,26 @@ class hitUnbundler:
                     item[key] = shared[key]
                 nOut += 1
                 yield item
-        print >>sys.stderr, "%s: %d => %d" % (self, nIn, nOut)
-        # print >>sys.stderr, "%s: %s" % (self, indexCount)
+        print >>sys.stderr, "%s: %d => %d" % ("unbundle", nIn, nOut)
 
+def adjustTimes (bundles):
+    groups = collections.defaultdict(list)
+    for b in bundles:
+        # Clip out the timezone, which is not reliably parsed by strptime
+        aTime, sTime = map(lambda t: time.mktime(datetime.datetime.strptime(t[:-8] + t[-5:], "%a %b %d %H:%M:%S %Y").timetuple()),
+                           (b["AcceptTime"], b["SubmitTime"]))
+        groups[b["WorkerId"]].append((sTime, aTime, b))
+    origWorktime = adjustedWorktime = 0
+    for workerID, bundles in groups.iteritems():
+        lastSubmit = 0
+        bundles.sort()
+        for i, (sTime, aTime, b) in enumerate(bundles):
+            b["AdjustedWorkTime"] = sTime - max(aTime, lastSubmit)
+            assert b["AdjustedWorkTime"] >= 0
+            # if b["AdjustedWorkTime"] == 0:
+            #     print >>sys.stderr, "Zero work time!!! (%s %s), (%s %s)" % (bundles[i - 1][-1]["AcceptTime"], bundles[i - 1][-1]["SubmitTime"], b["AcceptTime"], b["SubmitTime"])
+            lastSubmit = sTime
+        
 class tabItemWriter:
 
     def __init__ (self, file):
@@ -157,11 +192,16 @@ optparser.add_option("--json", action="store_true",
 
 (options, args) = optparser.parse_args()
 
-(infile, ) = args or (None, )
-infile = infile in ("-", None) and sys.stdin or open(infile, "r")
+# (infile, ) = args or (None, )
+# infile = infile in ("-", None) and sys.stdin or open(infile, "r")
 
-unbundler = hitUnbundler(batchFileReader(infile), burstplain=options.plain, addSequenceID=options.addseq)
+bundles = list(readBatchFile(fileinput.input(args)))
+adjustTimes(bundles)
+
+print >>sys.stderr, "Average adjusted worktime %.1fs" % (sum(b["AdjustedWorkTime"] for b in bundles)/(len(bundles) or 1))
+
+items = unbundleHITs(bundles, burstplain=options.plain, addSequenceID=options.addseq)
 writer = (options.json and jsonItemWriter or tabItemWriter)(sys.stdout)
-writer.writeAll(unbundler)
+writer.writeAll(items)
 
 ######################################################################
